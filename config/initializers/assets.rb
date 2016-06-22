1294f1d9dccc2c70189321f4a9d9012a73b60103
Vitaliy Babiychuk <bvvnbsp@gmail.com>
2016-06-22 10:32:31 +0300

    Precompile fonts and svg.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,15 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-#Rails.application.config.assets.precompile += %w( contact-form.js )
-#Rails.application.config.assets.precompile += %w( gmap3.min.js )
-#Rails.application.config.assets.precompile += %w( isotope.pkgd.min.js )
-#Rails.application.config.assets.precompile += %w( jquery.fitvids.js )
-#Rails.application.config.assets.precompile += %w( jquery.magnific.popup.min.js )
-#Rails.application.config.assets.precompile += %w( main.js )
-#Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
-#Rails.application.config.assets.precompile += %w( plagins.js )
-#Rails.application.config.assets.precompile += %w( smoothscroll.js )
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
