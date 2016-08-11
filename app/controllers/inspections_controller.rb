778f1c646cf91dead11bf78467a68147f8c34bef
Keti <ketevanzazarashvili.kz@gmail.com>
2016-08-11 21:06:22 +0100

    Allow users to search mechanics without signin/register

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,4 +1,5 @@
 class InspectionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_inspection, only: [:show, :edit, :update, :destroy]
   before_action :set_mechanic, only: [:new, :create]
 
